9d55ada96eda0c0c142a4bb560fe68c9d99dbbac
wades1248 <wades1248@gmail.com>
2019-03-13 00:32:06 -0500

    added screenshots, finalized layout and functionality

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-function toggle(){
+function toggleMenu(){
 	$(".toggle").click(function(event){
 		$(".toggle").toggleClass("active");
 		$(".nav-items").toggleClass("hidden");
 		}
 		)
 	}
-$(toggle());
+function enlargeImg(){
+	$(".enlarge").click(function(event){
+		$(".enlarge").toggleClass("bigJS");
+		}
+		)
+	}
+function initialize(){
+	toggleMenu();
+	enlargeImg();
+	}
+
+$(initialize());
